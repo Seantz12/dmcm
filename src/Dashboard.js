4aed35dc5574ae0775571344663c99280eea0430
Sean Zhou <seantxzhou12@gmail.com>
2019-04-12 23:35:58 -0400

    Dynamically add widgets

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -4,22 +4,30 @@ import Widget from './Widget'
 class Dashboard extends Component {
     constructor(props) {
         super(props);
-        this.state = { widgets: new Array() };
+        this.state = { widgets: [], count: 0 };
         this.createWidget = this.createWidget.bind(this);
         console.log(typeof(this.state.widgets))
     }
 
     createWidget() {
-        var widget = new Widget();
-        this.setState(state => (state.widgets.push(widget)));
+        var widget = new Widget({count: this.state.count});
+        this.setState(state => ({
+            widgets: state.widgets.concat([widget]),
+            count: ++state.count
+        }));
         console.log('created?')
+        console.log(this.state.widgets);
     }
 
     render() {
         return (
             <div className="dashboard">
                 <button onClick={this.createWidget}>Create Widget</button>
-                {this.state.widgets}
+                <div id="widgetLocation">
+                {this.state.widgets.map(widget => (
+                    <Widget count={0}/>
+                    ))} 
+                </div>
                 <h1> this is where i'd put my widgets</h1>
                 <h2> IF I HAD ANY </h2>
                 <h3> :(( </h3>
--- src/Widget.js
+++ src/Widget.js
@@ -3,13 +3,24 @@ import React, { Component } from 'react';
 class Widget extends Component {
     constructor(props) {
         super(props);
-        this.state = {app: null};
+        console.log('am i created ' + this.props.count);
+        this.state = {app: null, count: this.props.count};
+        this.updateWidget = this.updateWidget.bind(this);
+    }
+
+    updateWidget() {
+        this.setState(state => ({
+            count: ++state.count
+        }));
+        console.log(this.state.count);
     }
 
     render() {
+        console.log('test');
         return (
             <div className="widget">
-                <h1>this is a widget!</h1>
+                <h1>this is a widget! {this.state.count}</h1>
+                <button onClick={this.updateWidget}>click</button>
             </div>
         )
     }
